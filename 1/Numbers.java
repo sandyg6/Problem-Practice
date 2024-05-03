056461b2cb8cf3c1a7252e005ce50a2798e6641b
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-03 07:15:19 +0000

    commited changes

---
+++ 1/Numbers.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class Numbers{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        System.out.println("The extracted numbers are:");
+        while(number>0){
+            int rem = number%10;
+            System.out.print(rem + " ");
+            number/=10;
+        }
+        
+    }
+}
