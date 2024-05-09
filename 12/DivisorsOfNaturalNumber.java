220effde9ff1f0df84139a3484344a9ca3afd196
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-09 12:03:40 +0530

    Update DivisorsOfNaturalNumber.java

--- 12/DivisorsOfNaturalNumber.java
+++ 12/DivisorsOfNaturalNumber.java
@@ -6,11 +6,10 @@ public class DivisorsOfNaturalNumber {
       System.out.println("Enter the number :");
       int num = sc.nextInt();
      
-      for(int i = 1; i<num; i++) {
+      for(int i = 1; i<=num; i++) {
          if(num % i == 0) {
             System.out.print(i+" ");
          }
       }
-      System.out.print(num);
    }
 }
