7b986392f1f32ab3fa0737aa98ecdb25cef5c307
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-09 12:04:38 +0530

    Update DivisorsOfNaturalNumber.java

--- 12/DivisorsOfNaturalNumber.java
+++ 12/DivisorsOfNaturalNumber.java
@@ -5,7 +5,7 @@ public class DivisorsOfNaturalNumber {
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the number :");
       int num = sc.nextInt();
-     
+      System.out.println("The divisors are:");
       for(int i = 1; i<=num; i++) {
          if(num % i == 0) {
             System.out.print(i+" ");
