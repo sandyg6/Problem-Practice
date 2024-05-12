dcf2415be88ae8ff214c0df19a1123329b286b79
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-12 11:54:08 +0530

    Create 13.c

---
+++ 13/13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int findSecondLargest(int arr[], int size) {
+    int largest = arr[0];
+    int secondLargest = arr[0];
+
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return secondLargest;
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    printf("Enter %d elements of the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int secondLargest = findSecondLargest(arr, size);
+    printf("The second largest element in the array is: %d\n", secondLargest);
+    
+    return 0;
+}
