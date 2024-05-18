e2a84a56f659cdc5b9dbca1776f833dc5e16a92c
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-18 20:17:00 +0530

    Create 18.py

---
+++ 18/18.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first number of AP:"))
+d=int(input("Enter the difference of each number in AP:"))
+n=int(input("Enter the number of terms of AP need to be printed:"))
+
+sum_of_AP = (n//2)*(2*a + (n-1)*d)
+
+print("The sum of Arithmetic progression:",sum_of_AP)
+
+    
+
