ffa1e1bcf0dbbe75df1440f19a0ca0fbc3b8915a
SANDHIYA G <118624179+sandyg6@users.noreply.github.com>
2024-05-26 13:36:39 +0000

    Commit changes

--- 22/FactofNum.java
+++ 22/FactofNum.java
@@ -4,12 +4,12 @@ class FactofNum {
     public static void main(String[] args) {
         int n,fact=1;
         System.out.println("Enter a number:");
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            fact *= i;
+        try (Scanner sc = new Scanner(System.in)) {
+            n = sc.nextInt();
+            for(int i=1;i<=n;i++){
+                fact *= i;
+            }
+            System.out.println("The factorial of "+n+" is "+fact);
         }
-        System.out.println("The factorial of "+n+" is "+fact);
-        sc.close();
     }
 }
